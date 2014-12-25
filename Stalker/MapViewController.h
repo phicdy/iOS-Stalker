a511c895c7230c033f4e50a3e5926e71c566b44e
pluea <pluea.ky@gmail.com>
2014-12-25 16:36:30 +0900

    Add Map View

---
+++ Stalker/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  Stalker
+//
+//  Created by Keisuke Yamaguchi on 2014/12/25.
+//  Copyright (c) 2014年 Keisuke Yamaguchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
---
+++ Stalker/MapViewController.m
@@ -0,0 +1,22 @@
+//
+//  MapViewController.m
+//  Stalker
+//
+//  Created by Keisuke Yamaguchi on 2014/12/25.
+//  Copyright (c) 2014年 Keisuke Yamaguchi. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
