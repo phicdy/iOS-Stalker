3573051d052d082c5b4405b6a3ed93343d39186c
pluea <pluea.ky@gmail.com>
2014-12-25 15:49:33 +0900

    Initialize location manager

--- Stalker/ViewController.h
+++ Stalker/ViewController.h
@@ -11,6 +11,7 @@
 
 @interface ViewController : UIViewController <CLLocationManagerDelegate>
 
+- (void)startStandardUpdates;
 
 @end
 
--- Stalker/ViewController.m
+++ Stalker/ViewController.m
@@ -10,18 +10,34 @@
 
 @interface ViewController ()
 
+@property CLLocationManager* locationManager;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self startStandardUpdates];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)startStandardUpdates {
+    // Create the location manager if this object does not
+    // already have one.
+    if (nil == _locationManager)
+        _locationManager = [[CLLocationManager alloc] init];
+    _locationManager.delegate = self;
+    _locationManager.desiredAccuracy = kCLLocationAccuracyKilometer;
+    // Set a movement threshold for new events.
+//    _locationManager.distanceFilter = 500; // meters
+    [_locationManager startUpdatingLocation];
 }
 
 @end
