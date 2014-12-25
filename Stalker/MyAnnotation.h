f1509ccf2e63d6de93477205178c82979b765819
pluea <pluea.ky@gmail.com>
2014-12-25 19:53:40 +0900

    Add my annotation

---
+++ Stalker/MyAnnotation.h
@@ -0,0 +1,14 @@
+//
+//  MyAnnotation.h
+//  Stalker
+//
+//  Created by Keisuke Yamaguchi on 2014/12/25.
+//  Copyright (c) 2014年 Keisuke Yamaguchi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface MyAnnotation : NSObject <MKAnnotation>
+
+@end
---
+++ Stalker/MyAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  MyAnnotation.m
+//  Stalker
+//
+//  Created by Keisuke Yamaguchi on 2014/12/25.
+//  Copyright (c) 2014年 Keisuke Yamaguchi. All rights reserved.
+//
+
+#import "MyAnnotation.h"
+
+@implementation MyAnnotation
+
+@end
