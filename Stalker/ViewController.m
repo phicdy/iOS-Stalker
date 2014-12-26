bbda6a0aa2101c7b1c608621b3a1e51ee04228b9
pluea <pluea.ky@gmail.com>
2014-12-26 17:07:06 +0900

    Disable filter

--- Stalker/ViewController.m
+++ Stalker/ViewController.m
@@ -38,7 +38,7 @@
     if (nil == _locationManager)
         _locationManager = [[CLLocationManager alloc] init];
     _locationManager.delegate = self;
-    _locationManager.desiredAccuracy = kCLLocationAccuracyKilometer;
+    _locationManager.desiredAccuracy = kCLDistanceFilterNone;
     // Set a movement threshold for new events.
     _locationManager.distanceFilter = 10; // meters
     [_locationManager startUpdatingLocation];
